dec51ca29adb78c12548174de2cb5fbe9f9eed13
Алексей Бельченков <u608110@gmail.com>
2022-10-27 19:32:48 +0400

    Add assets and logo

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,30 @@
 import './Navbar.scss';
+import { images } from '../../constants';
 
 const Navbar = () => {
+    const menuList = [
+        'home',
+        'about',
+        'contact',
+        'work',
+        'skills',
+    ];
+
     return (
-        <div>
-            Navbar
-        </div>
+        <nav className="app__navbar">
+            <div className="app__navbar-logo">
+                <img src={images.logo} alt="Logo" />
+            </div>
+            <ul className="app__navbar-links">
+                { menuList.map(item => (
+                    <li key={`link-${item}`} className="app__flex p-text">
+                        <div />
+                        <a href={`#${item}`}>{item}</a>
+                    </li>
+                    )
+                )}
+            </ul>
+        </nav>
     );
 };
 
