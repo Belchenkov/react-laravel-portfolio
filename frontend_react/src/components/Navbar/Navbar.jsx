e1821c3acf8e6abdb7c670050bff037cd88fc330
Алексей Бельченков <u608110@gmail.com>
2022-10-28 18:05:58 +0400

    Finish navbar section

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,12 @@
+import { useState } from 'react';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
+
 import './Navbar.scss';
 import { images } from '../../constants';
 
 const Navbar = () => {
+    const [toggle, setToggle] = useState(false);
     const menuList = [
         'home',
         'about',
@@ -24,6 +29,33 @@ const Navbar = () => {
                     )
                 )}
             </ul>
+            <div className="app__navbar-menu">
+                <HiMenuAlt4
+                    onClick={() => setToggle(true)}
+                />
+                {
+                    toggle && (
+                        <motion.div
+                            whileInView={{ x: [300, 0] }}
+                            transition={{ duration: .85, ease: 'easeOut' }}
+                        >
+                            <HiX onClick={() => setToggle(false)} />
+
+                            <ul>
+                                { menuList.map(item => (
+                                        <li key={item} className="app__flex p-text">
+                                            <a
+                                                href={`#${item}`}
+                                                onClick={() => setToggle(false)}
+                                            >{item}</a>
+                                        </li>
+                                    )
+                                )}
+                            </ul>
+                        </motion.div>
+                    )
+                }
+            </div>
         </nav>
     );
 };
