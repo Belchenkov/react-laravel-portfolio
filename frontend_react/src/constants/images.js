df8a65797f8671922286d665a2aa9293755e4d7a
Алексей Бельченков <u608110@gmail.com>
2022-11-01 18:24:05 +0400

    Make header html

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -34,8 +34,10 @@ import bolt from '../assets/bolt.png';
 import nb from '../assets/nb.png';
 import skype from '../assets/skype.png';
 import spotify from '../assets/spotify.png';
+import avatar from '../assets/avatar.png';
 
 export default {
+    avatar,
     email,
     mobile,
     api,
