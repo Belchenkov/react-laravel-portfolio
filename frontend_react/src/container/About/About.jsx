4ab3b4133cb5efe6379b90977eea4ac747102ca4
Алексей Бельченков <u608110@gmail.com>
2022-11-08 18:06:28 +0400

    About Section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,10 +1,45 @@
+import { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+
 import './About.scss';
+import { images } from '../../constants';
+
+const abouts = [
+    { title: 'Backend Development', description: '', imgUrl: images.about01 },
+    { title: 'Frontend Development', description: '', imgUrl: images.about02 },
+    { title: 'Mobile Development', description: '', imgUrl: images.about03 },
+    { title: 'Desktop Development', description: '', imgUrl: images.about04 }
+];
 
 const About = () => {
     return (
-        <div>
-            About
-        </div>
+        <>
+            <h2 className="head-text">I know that  <span>Good Apps</span>
+                <br/>
+                means <span>Good Business</span>
+            </h2>
+            <div className="app__profiles">
+                {
+                    abouts.map((about, index) => (
+                        <motion.div
+                            whileInView={{ opacity: 1 }}
+                            whileHover={{ scale: 1.1 }}
+                            transition={{ duration: .5, type: 'tween' }}
+                            className="app__profile-item"
+                            key={about.title + index}
+                        >
+                            <img src={about.imgUrl} alt={about.title} />
+                            <h2 className="bold-text" style={{ marginTop: 20 }}>
+                                {about.title}
+                            </h2>
+                            <p className="p-text" style={{ marginTop: 10 }}>
+                                {about.description}
+                            </p>
+                        </motion.div>
+                    ))
+                }
+            </div>
+        </>
     );
 };
 
