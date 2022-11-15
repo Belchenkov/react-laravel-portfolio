8c46b224a97515f228a738ef23d8276ca8348eb1
Алексей Бельченков <u608110@gmail.com>
2022-11-15 18:31:28 +0400

    About Section output db data

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -3,15 +3,19 @@ import { motion } from 'framer-motion';
 
 import './About.scss';
 import { images } from '../../constants';
-
-const abouts = [
-    { title: 'Backend Development', description: '', imgUrl: images.about01 },
-    { title: 'Frontend Development', description: '', imgUrl: images.about02 },
-    { title: 'Mobile Development', description: '', imgUrl: images.about03 },
-    { title: 'Desktop Development', description: '', imgUrl: images.about04 }
-];
+import { urlFor, client } from '../../client';
 
 const About = () => {
+    const [abouts, setAbouts] = useState([]);
+
+    useEffect(() => {
+        const query = '*[_type == "abouts"]';
+
+        client.fetch(query)
+            .then(data => setAbouts(data))
+            .catch(err => console.error(err));
+    }, []);
+
     return (
         <>
             <h2 className="head-text">I know that  <span>Good Apps</span>
@@ -28,7 +32,7 @@ const About = () => {
                             className="app__profile-item"
                             key={about.title + index}
                         >
-                            <img src={about.imgUrl} alt={about.title} />
+                            <img src={urlFor(about.imgUrl)} alt={about.title} />
                             <h2 className="bold-text" style={{ marginTop: 20 }}>
                                 {about.title}
                             </h2>
